2c4951ca553773fa639a229f905105246958cf53
bitirimer0 <erolsnl@gmail.com>
2017-09-06 05:37:32 +0300

    Update 1

--- SokakCeteleriGameBot/ElementHelper.cs
+++ SokakCeteleriGameBot/ElementHelper.cs
@@ -29,5 +29,200 @@ namespace SokakCeteleriGameBot
             }
             return null;
         }
+
+        public static HtmlElement FindRespectElement(HtmlElement document)
+        {
+            var elements = document.GetElementsByTagName("div");
+            foreach (HtmlElement e in elements)
+            {
+                if (e.GetAttribute("className").Contains("b-left-links"))
+                {
+                    return e;
+                }
+            }
+            return null;
+        }
+
+        public static HtmlElement FindTopMenuElement(HtmlElement document)
+        {
+            var elements = document.GetElementsByTagName("div");
+            foreach (HtmlElement e in elements)
+            {
+                if (e.GetAttribute("className").Contains("local-tasks"))
+                {
+                    return e;
+                }
+            }
+            return null;
+        }
+
+        public static HtmlElement FindFightSearchElement(HtmlElement document)
+        {
+            var elements = document.GetElementsByTagName("div");
+            foreach (HtmlElement e in elements)
+            {
+                if (e.GetAttribute("className").Contains("body fight-finder modfaderacket fight-finder-enhanced"))
+                {
+                    return e;
+                }
+            }
+            return null;
+        }
+
+        public static HtmlElement FindInventoryElement(HtmlElement document)
+        {
+            var elements = document.GetElementsByTagName("div");
+            foreach (HtmlElement e in elements)
+            {
+                if (e.GetAttribute("className").Contains("usrtools"))
+                {
+                    return e;
+                }
+            }
+            return null;
+        }
+
+        public static HtmlElement FindHomeElement(HtmlElement document)
+        {
+            var elements = document.GetElementsByTagName("div");
+            foreach (HtmlElement e in elements)
+            {
+                if (e.GetAttribute("className").Contains("main-menu"))
+                {
+                    return e;
+                }
+            }
+            return null;
+        }
+
+        public static HtmlElement FindStreetElement(HtmlElement document)
+        {
+            var elements = document.GetElementsByTagName("div");
+            foreach (HtmlElement e in elements)
+            {
+                if (e.GetAttribute("className").Contains("private-map-10 my-map"))
+                {
+                    return e;
+                }
+            }
+            return null;
+        }
+
+        public static HtmlElement FindDrinkElement(HtmlElement document)
+        {
+            var elements = document.GetElementsByTagName("div");
+            foreach (HtmlElement e in elements)
+            {
+                if (e.GetAttribute("className").Contains("inventory_slotsw"))
+                {
+                    return e;
+                }
+            }
+            return null;
+        }
+
+        public static HtmlElement FindTributeElement(HtmlElement document)
+        {
+            var elements = document.GetElementsByTagName("table");
+            foreach (HtmlElement e in elements)
+            {
+                if (e.GetAttribute("className").Contains("default"))
+                {
+                    return e;
+                }
+            }
+            return null;
+        }
+        //Çalışmnaya bilir 
+        public static HtmlElement FindQuestElement(HtmlElement document)
+        {
+            var elements = document.GetElementsByTagName("table").GetElementsByName("qListContainer");
+            foreach (HtmlElement e in elements)
+            {
+                if (e != null)
+                {
+                    return e.Children[0];
+                }
+            }
+            return null;
+        }
+
+        public static HtmlElement FindPrisonElement(HtmlElement document)
+        {
+            var elements = document.GetElementsByTagName("form").GetElementsByName("arrested");
+            foreach (HtmlElement e in elements)
+            {
+                if (e != null)
+                {
+                    return e;
+                }
+            }
+            return null;
+        }
+
+        public static HtmlElement FindHospitalElement(HtmlElement document)
+        {
+            var elements = document.GetElementsByTagName("div");
+            foreach (HtmlElement e in elements)
+            {
+                if (e.GetAttribute("className").Contains("modhospital"))
+                {
+                    return e;
+                }
+            }
+            return null;
+        }
+
+        public static HtmlElement FindTrainingContentElement(HtmlElement document)
+        {
+            var elements = document.GetElementsByTagName("div");
+            foreach (HtmlElement e in elements)
+            {
+                if (e.GetAttribute("className").Contains("training_content"))
+                {
+                    return e;
+                }
+            }
+            return null;
+        }
+
+        public static HtmlElement FindGangHomeElement(HtmlElement document)
+        {
+            var elements = document.GetElementsByTagName("div");
+            foreach (HtmlElement e in elements)
+            {
+                if (e.GetAttribute("className").Contains("body  mygang"))
+                {
+                    return e;
+                }
+            }
+            return null;
+        }
+
+        public static HtmlElement FindGangQuestElement(HtmlElement document)
+        {
+            var elements = document.GetElementsByTagName("table");
+            foreach (HtmlElement e in elements)
+            {
+                if (e.GetAttribute("className").Contains("gcrime_list"))
+                {
+                    return e;
+                }
+            }
+            return null;
+        }
+
+        public static HtmlElement FindGangCrimeElement(HtmlElement document)
+        {
+            var elements = document.GetElementsByTagName("div");
+            foreach (HtmlElement e in elements)
+            {
+                if (e.GetAttribute("className").Contains("body"))
+                {
+                    return e;
+                }
+            }
+            return null;
+        }
     }
 }
--- SokakCeteleriGameBot/MainForm.Designer.cs
+++ SokakCeteleriGameBot/MainForm.Designer.cs
@@ -39,63 +39,95 @@
             this.txtUserName = new System.Windows.Forms.TextBox();
             this.txtPassword = new System.Windows.Forms.TextBox();
             this.startPanel = new System.Windows.Forms.Panel();
+            this.cbHastaneKart = new System.Windows.Forms.CheckBox();
+            this.cbHapisKart = new System.Windows.Forms.CheckBox();
+            this.lbCazibe = new System.Windows.Forms.Label();
+            this.lbGuc = new System.Windows.Forms.Label();
+            this.lbZeka = new System.Windows.Forms.Label();
+            this.lbAtak = new System.Windows.Forms.Label();
+            this.lbPara = new System.Windows.Forms.Label();
+            this.lbSaygi = new System.Windows.Forms.Label();
+            this.lbSeviye = new System.Windows.Forms.Label();
+            this.lbCazibePuan = new System.Windows.Forms.Label();
+            this.lbGucPuan = new System.Windows.Forms.Label();
+            this.lbZekaPuan = new System.Windows.Forms.Label();
+            this.lbSavasPuan = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             this.startPanel.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
             // 
+            this.panel1.Controls.Add(this.lbCazibePuan);
+            this.panel1.Controls.Add(this.lbGucPuan);
+            this.panel1.Controls.Add(this.lbZekaPuan);
+            this.panel1.Controls.Add(this.lbSavasPuan);
+            this.panel1.Controls.Add(this.lbAtak);
+            this.panel1.Controls.Add(this.lbPara);
+            this.panel1.Controls.Add(this.lbSaygi);
+            this.panel1.Controls.Add(this.lbSeviye);
+            this.panel1.Controls.Add(this.lbCazibe);
+            this.panel1.Controls.Add(this.lbGuc);
+            this.panel1.Controls.Add(this.lbZeka);
+            this.panel1.Controls.Add(this.cbHapisKart);
+            this.panel1.Controls.Add(this.cbHastaneKart);
             this.panel1.Controls.Add(this.lbZehir);
             this.panel1.Controls.Add(this.lbRisk);
             this.panel1.Controls.Add(this.lbEnerji);
             this.panel1.Controls.Add(this.lbCan);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(1093, 188);
+            this.panel1.Size = new System.Drawing.Size(1405, 122);
             this.panel1.TabIndex = 0;
             // 
             // lbZehir
             // 
             this.lbZehir.AutoSize = true;
-            this.lbZehir.Location = new System.Drawing.Point(12, 118);
+            this.lbZehir.Location = new System.Drawing.Point(8, 77);
+            this.lbZehir.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lbZehir.Name = "lbZehir";
-            this.lbZehir.Size = new System.Drawing.Size(57, 20);
+            this.lbZehir.Size = new System.Drawing.Size(40, 13);
             this.lbZehir.TabIndex = 0;
             this.lbZehir.Text = "Zehir : ";
             // 
             // lbRisk
             // 
             this.lbRisk.AutoSize = true;
-            this.lbRisk.Location = new System.Drawing.Point(12, 87);
+            this.lbRisk.Location = new System.Drawing.Point(8, 57);
+            this.lbRisk.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lbRisk.Name = "lbRisk";
-            this.lbRisk.Size = new System.Drawing.Size(52, 20);
+            this.lbRisk.Size = new System.Drawing.Size(37, 13);
             this.lbRisk.TabIndex = 0;
             this.lbRisk.Text = "Risk : ";
             // 
             // lbEnerji
             // 
             this.lbEnerji.AutoSize = true;
-            this.lbEnerji.Location = new System.Drawing.Point(12, 55);
+            this.lbEnerji.Location = new System.Drawing.Point(8, 36);
+            this.lbEnerji.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lbEnerji.Name = "lbEnerji";
-            this.lbEnerji.Size = new System.Drawing.Size(61, 20);
+            this.lbEnerji.Size = new System.Drawing.Size(42, 13);
             this.lbEnerji.TabIndex = 0;
             this.lbEnerji.Text = "Enerji : ";
             // 
             // lbCan
             // 
             this.lbCan.AutoSize = true;
-            this.lbCan.Location = new System.Drawing.Point(12, 24);
+            this.lbCan.Location = new System.Drawing.Point(8, 16);
+            this.lbCan.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lbCan.Name = "lbCan";
-            this.lbCan.Size = new System.Drawing.Size(50, 20);
+            this.lbCan.Size = new System.Drawing.Size(35, 13);
             this.lbCan.TabIndex = 0;
             this.lbCan.Text = "Can : ";
             // 
             // btnStart
             // 
-            this.btnStart.Location = new System.Drawing.Point(188, 79);
+            this.btnStart.Location = new System.Drawing.Point(125, 51);
+            this.btnStart.Margin = new System.Windows.Forms.Padding(2);
             this.btnStart.Name = "btnStart";
-            this.btnStart.Size = new System.Drawing.Size(133, 41);
+            this.btnStart.Size = new System.Drawing.Size(89, 27);
             this.btnStart.TabIndex = 1;
             this.btnStart.Text = " Başla";
             this.btnStart.UseVisualStyleBackColor = true;
@@ -104,33 +136,37 @@
             // lbUserName
             // 
             this.lbUserName.AutoSize = true;
-            this.lbUserName.Location = new System.Drawing.Point(18, 20);
+            this.lbUserName.Location = new System.Drawing.Point(12, 13);
+            this.lbUserName.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lbUserName.Name = "lbUserName";
-            this.lbUserName.Size = new System.Drawing.Size(93, 20);
+            this.lbUserName.Size = new System.Drawing.Size(64, 13);
             this.lbUserName.TabIndex = 0;
             this.lbUserName.Text = "Kullanici Adi";
             // 
             // lbPassword
             // 
             this.lbPassword.AutoSize = true;
-            this.lbPassword.Location = new System.Drawing.Point(69, 50);
+            this.lbPassword.Location = new System.Drawing.Point(46, 32);
+            this.lbPassword.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lbPassword.Name = "lbPassword";
-            this.lbPassword.Size = new System.Drawing.Size(42, 20);
+            this.lbPassword.Size = new System.Drawing.Size(28, 13);
             this.lbPassword.TabIndex = 0;
             this.lbPassword.Text = "Sifre";
             // 
             // txtUserName
             // 
-            this.txtUserName.Location = new System.Drawing.Point(117, 17);
+            this.txtUserName.Location = new System.Drawing.Point(78, 11);
+            this.txtUserName.Margin = new System.Windows.Forms.Padding(2);
             this.txtUserName.Name = "txtUserName";
-            this.txtUserName.Size = new System.Drawing.Size(204, 26);
+            this.txtUserName.Size = new System.Drawing.Size(137, 20);
             this.txtUserName.TabIndex = 2;
             // 
             // txtPassword
             // 
-            this.txtPassword.Location = new System.Drawing.Point(117, 47);
+            this.txtPassword.Location = new System.Drawing.Point(78, 31);
+            this.txtPassword.Margin = new System.Windows.Forms.Padding(2);
             this.txtPassword.Name = "txtPassword";
-            this.txtPassword.Size = new System.Drawing.Size(204, 26);
+            this.txtPassword.Size = new System.Drawing.Size(137, 20);
             this.txtPassword.TabIndex = 3;
             // 
             // startPanel
@@ -140,19 +176,151 @@
             this.startPanel.Controls.Add(this.btnStart);
             this.startPanel.Controls.Add(this.txtUserName);
             this.startPanel.Controls.Add(this.lbPassword);
-            this.startPanel.Location = new System.Drawing.Point(12, 194);
+            this.startPanel.Location = new System.Drawing.Point(8, 126);
+            this.startPanel.Margin = new System.Windows.Forms.Padding(2);
             this.startPanel.Name = "startPanel";
-            this.startPanel.Size = new System.Drawing.Size(350, 137);
+            this.startPanel.Size = new System.Drawing.Size(233, 89);
             this.startPanel.TabIndex = 4;
             // 
-            // Form1
+            // cbHastaneKart
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.cbHastaneKart.AutoSize = true;
+            this.cbHastaneKart.Location = new System.Drawing.Point(371, 15);
+            this.cbHastaneKart.Name = "cbHastaneKart";
+            this.cbHastaneKart.Size = new System.Drawing.Size(132, 17);
+            this.cbHastaneKart.TabIndex = 1;
+            this.cbHastaneKart.Text = "Hastanede Kart Kullan";
+            this.cbHastaneKart.UseVisualStyleBackColor = true;
+            // 
+            // cbHapisKart
+            // 
+            this.cbHapisKart.AutoSize = true;
+            this.cbHapisKart.Location = new System.Drawing.Point(371, 39);
+            this.cbHapisKart.Name = "cbHapisKart";
+            this.cbHapisKart.Size = new System.Drawing.Size(116, 17);
+            this.cbHapisKart.TabIndex = 2;
+            this.cbHapisKart.Text = "Hapiste Kart Kullan";
+            this.cbHapisKart.UseVisualStyleBackColor = true;
+            // 
+            // lbCazibe
+            // 
+            this.lbCazibe.AutoSize = true;
+            this.lbCazibe.Location = new System.Drawing.Point(174, 57);
+            this.lbCazibe.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lbCazibe.Name = "lbCazibe";
+            this.lbCazibe.Size = new System.Drawing.Size(48, 13);
+            this.lbCazibe.TabIndex = 4;
+            this.lbCazibe.Text = "Cazibe : ";
+            // 
+            // lbGuc
+            // 
+            this.lbGuc.AutoSize = true;
+            this.lbGuc.Location = new System.Drawing.Point(174, 36);
+            this.lbGuc.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lbGuc.Name = "lbGuc";
+            this.lbGuc.Size = new System.Drawing.Size(36, 13);
+            this.lbGuc.TabIndex = 5;
+            this.lbGuc.Text = "Güç : ";
+            // 
+            // lbZeka
+            // 
+            this.lbZeka.AutoSize = true;
+            this.lbZeka.Location = new System.Drawing.Point(174, 16);
+            this.lbZeka.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lbZeka.Name = "lbZeka";
+            this.lbZeka.Size = new System.Drawing.Size(41, 13);
+            this.lbZeka.TabIndex = 6;
+            this.lbZeka.Text = "Zeka : ";
+            // 
+            // lbAtak
+            // 
+            this.lbAtak.AutoSize = true;
+            this.lbAtak.Location = new System.Drawing.Point(83, 77);
+            this.lbAtak.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lbAtak.Name = "lbAtak";
+            this.lbAtak.Size = new System.Drawing.Size(38, 13);
+            this.lbAtak.TabIndex = 7;
+            this.lbAtak.Text = "Atak : ";
+            // 
+            // lbPara
+            // 
+            this.lbPara.AutoSize = true;
+            this.lbPara.Location = new System.Drawing.Point(83, 57);
+            this.lbPara.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lbPara.Name = "lbPara";
+            this.lbPara.Size = new System.Drawing.Size(38, 13);
+            this.lbPara.TabIndex = 8;
+            this.lbPara.Text = "Para : ";
+            // 
+            // lbSaygi
+            // 
+            this.lbSaygi.AutoSize = true;
+            this.lbSaygi.Location = new System.Drawing.Point(83, 36);
+            this.lbSaygi.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lbSaygi.Name = "lbSaygi";
+            this.lbSaygi.Size = new System.Drawing.Size(42, 13);
+            this.lbSaygi.TabIndex = 9;
+            this.lbSaygi.Text = "Saygı : ";
+            // 
+            // lbSeviye
+            // 
+            this.lbSeviye.AutoSize = true;
+            this.lbSeviye.Location = new System.Drawing.Point(83, 16);
+            this.lbSeviye.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lbSeviye.Name = "lbSeviye";
+            this.lbSeviye.Size = new System.Drawing.Size(48, 13);
+            this.lbSeviye.TabIndex = 10;
+            this.lbSeviye.Text = "Seviye : ";
+            // 
+            // lbCazibePuan
+            // 
+            this.lbCazibePuan.AutoSize = true;
+            this.lbCazibePuan.Location = new System.Drawing.Point(256, 77);
+            this.lbCazibePuan.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lbCazibePuan.Name = "lbCazibePuan";
+            this.lbCazibePuan.Size = new System.Drawing.Size(78, 13);
+            this.lbCazibePuan.TabIndex = 11;
+            this.lbCazibePuan.Text = "Cazibe Puanı : ";
+            // 
+            // lbGucPuan
+            // 
+            this.lbGucPuan.AutoSize = true;
+            this.lbGucPuan.Location = new System.Drawing.Point(256, 57);
+            this.lbGucPuan.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lbGucPuan.Name = "lbGucPuan";
+            this.lbGucPuan.Size = new System.Drawing.Size(66, 13);
+            this.lbGucPuan.TabIndex = 12;
+            this.lbGucPuan.Text = "Güç Puanı : ";
+            // 
+            // lbZekaPuan
+            // 
+            this.lbZekaPuan.AutoSize = true;
+            this.lbZekaPuan.Location = new System.Drawing.Point(256, 36);
+            this.lbZekaPuan.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lbZekaPuan.Name = "lbZekaPuan";
+            this.lbZekaPuan.Size = new System.Drawing.Size(71, 13);
+            this.lbZekaPuan.TabIndex = 13;
+            this.lbZekaPuan.Text = "Zeka Puanı : ";
+            // 
+            // lbSavasPuan
+            // 
+            this.lbSavasPuan.AutoSize = true;
+            this.lbSavasPuan.Location = new System.Drawing.Point(256, 16);
+            this.lbSavasPuan.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lbSavasPuan.Name = "lbSavasPuan";
+            this.lbSavasPuan.Size = new System.Drawing.Size(76, 13);
+            this.lbSavasPuan.TabIndex = 14;
+            this.lbSavasPuan.Text = "Savaş Puanı : ";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1093, 557);
+            this.ClientSize = new System.Drawing.Size(1405, 752);
             this.Controls.Add(this.startPanel);
             this.Controls.Add(this.panel1);
-            this.Name = "Form1";
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "MainForm";
             this.Text = "Form1";
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
@@ -175,6 +343,19 @@
         private System.Windows.Forms.TextBox txtUserName;
         private System.Windows.Forms.TextBox txtPassword;
         private System.Windows.Forms.Panel startPanel;
+        private System.Windows.Forms.Label lbCazibePuan;
+        private System.Windows.Forms.Label lbGucPuan;
+        private System.Windows.Forms.Label lbZekaPuan;
+        private System.Windows.Forms.Label lbSavasPuan;
+        private System.Windows.Forms.Label lbAtak;
+        private System.Windows.Forms.Label lbPara;
+        private System.Windows.Forms.Label lbSaygi;
+        private System.Windows.Forms.Label lbSeviye;
+        private System.Windows.Forms.Label lbCazibe;
+        private System.Windows.Forms.Label lbGuc;
+        private System.Windows.Forms.Label lbZeka;
+        private System.Windows.Forms.CheckBox cbHapisKart;
+        private System.Windows.Forms.CheckBox cbHastaneKart;
     }
 }
 
